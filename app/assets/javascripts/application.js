65d6dc121daa05d3f229e3033f58c32b8cdbce72
Ymow Wu <kenshin0812@hotmail.com>
2014-01-23 20:03:36 +0800

    img_tag

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require responsive-nav
+//= require glide
+//= require bootstrap
 //= require twitter/bootstrap/alert
 //= require twitter/bootstrap/dropdown
